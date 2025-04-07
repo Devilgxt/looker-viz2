c6dac60d014bbad6ee347bfc8a5891f3beeca15e
Devilgxt <147392947+Devilgxt@users.noreply.github.com>
2025-04-07 14:43:41 +0200

    Update manifest.js

--- manifest.js
+++ manifest.js
@@ -1,11 +1,14 @@
-// manifest.js
-__lookerjsManifest = {
-  name: "My Custom Viz",
-  description: "Visualizzazione personalizzata per Looker Studio",
-  style: "index.css",
-  script: "index.js",
-  data: {
-    url: "index.html"
+const manifest = {
+  "name": "My Custom Viz",
+  "description": "Visualizzazione personalizzata per Looker Studio",
+  "style": "index.css",
+  "script": "index.js",
+  "data": {
+    "url": "index.html"
   },
-  icon: "https://lookerstudio.google.com/images/visualization.svg"
+  "icon": "https://lookerstudio.google.com/images/visualization.svg"
 };
+
+if (typeof module !== "undefined") {
+  module.exports = manifest;  // Export for use in other modules
+}
