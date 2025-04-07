3eaadf2f930600de31039fabffe578a6470c7ffc
Devilgxt <147392947+Devilgxt@users.noreply.github.com>
2025-04-07 14:35:29 +0200

    Add files via upload

---
+++ manifest.js
@@ -0,0 +1,11 @@
+// manifest.js
+__lookerjsManifest = {
+  name: "My Custom Viz",
+  description: "Visualizzazione personalizzata per Looker Studio",
+  style: "index.css",
+  script: "index.js",
+  data: {
+    url: "index.html"
+  },
+  icon: "https://lookerstudio.google.com/images/visualization.svg"
+};
